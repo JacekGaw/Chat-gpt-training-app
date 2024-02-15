66b5f5b45e210d2a64c03cff573454226b69263d
JacekGaw <jac.gawlyta@gmail.com>
2024-02-15 21:45:16 +0100

    Add saving history of conversation to file, now chat can keep context

--- server/openai-config.js
+++ server/openai-config.js
@@ -1,28 +1,58 @@
 import { config } from "dotenv";
+import * as fs from "fs";
 config();
-
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env["OPENAI_API_KEY"], // This is the default and can be omitted
+  apiKey: process.env["OPENAI_API_KEY"],
 });
 
+const filePath = "history.json";
+
 async function main(userMessage) {
-  let contentInput = "Say that this is test";
-  if(userMessage) {
-    contentInput = userMessage;
+  const messagesToChat = await checkIsHistoryFile();
+  let userMessageJson = {};
+
+  if (userMessage) {
+    userMessageJson = { role: "user", content: userMessage };
+  } else {
+    const contentInput = "Say that this is a test";
+    userMessageJson = { role: "user", content: contentInput };
   }
+  let mess = [...messagesToChat, userMessageJson]
   const completion = await openai.chat.completions.create({
-    messages: [
-      {role: 'system', content: "You are a helpful assistant designed to output JSON."},
-      { role: "user", content: contentInput }
-      
-    ],
+    messages: mess,
     model: "gpt-3.5-turbo",
   });
 
-  console.log(completion.choices[0].message);
-  return completion.choices[0].message;
+  const generatedMessage = completion.choices[0].message;
+  writeToFile([...messagesToChat, userMessageJson, generatedMessage]);
+
+  return generatedMessage;
+}
+
+async function writeToFile(toWrite) {
+  const updatedData = JSON.stringify(toWrite, null, 2);
+
+  fs.writeFileSync(filePath, updatedData, "utf8");
+  console.log("Conversation history has been written to the file.");
+}
+
+async function checkIsHistoryFile() {
+  let historyData = [];
+
+  if (fs.existsSync(filePath)) {
+    console.log("File exists. Reading history...");
+    const data = fs.readFileSync(filePath, "utf-8");
+    historyData = JSON.parse(data);
+  } else {
+    console.log("File does not exist. Creating a new one...");
+    const initialData = [{ role: "system", content: "You are a helpful assistant that return JSON" }];
+    fs.writeFileSync(filePath, JSON.stringify(initialData, null, 2), "utf8");
+    historyData = initialData;
+  }
+
+  return historyData;
 }
 
 export default main;
